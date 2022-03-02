1c573c577e200eccc48fec4ea73a83bc82f5ec40
leibaio <leibaio@qq.com>
2022-03-02 19:35:04 +0800

    数组去重

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-function outer(){
-
-  var num = 0;
-
-   function inner(){
-
-        num++;
-
-        console.log(num);
-
+// 对象克隆 支持基本数据类型及对象 递归
+function clone(obj) {
+  var o;
+  switch (typeof obj) {
+    case "undefined":
+      break;
+    case "string": o = obj + "";
+      break;
+    case "number": o = obj - 0;
+      break;
+    case "boolean": o = obj;
+      break;
+    case "object": //分为 对象（Object）和 数组（Array）
+      if (obj === null) {
+        o = null;
+      } else {
+        if (Object.prototype.toString.call(obj).slice(8, -1) === "Array") {
+          o = [];
+          for (var i = 0; i < obj.length; i++) {
+            o.push(clone(obj[i]));
+          }
+        } else {
+          o = {};
+          for (var k in obj) {
+            o[k] = clone(obj[k]);
+          }
+        }
+      }
+      break;
+    default: o = obj;
+      break;
   }
-
-      return inner;
-
+  return o;
 }
 
-var f1 = outer();
-
-f1();
-
-f1();
-
-var f2 = outer();
-
-f2();
-
-f2();
+var m1 = clone([1, 2, 3]);
+var m2 = clone('1', 'hello');
+console.log(m1);
+console.log(m2);
--- interview/FEInterview.md
+++ interview/FEInterview.md
@@ -45,5 +45,45 @@
 #### 实现一个函数 clone 可以对 js 中五种主要数据类型进行复制
 
 ```js
+// 对象克隆 支持基本数据类型及对象 递归
+function clone(obj) {
+  var o;
+  switch (typeof obj) {
+    case "undefined":
+      break;
+    case "string": o = obj + "";
+      break;
+    case "number": o = obj - 0;
+      break;
+    case "boolean": o = obj;
+      break;
+    case "object": //分为 对象（Object）和 数组（Array）
+      if (obj === null) {
+        o = null;
+      } else {
+        if (Object.prototype.toString.call(obj).slice(8, -1) === "Array") {
+          o = [];
+          for (var i = 0; i < obj.length; i++) {
+            o.push(clone(obj[i]));
+          }
+        } else {
+          o = {};
+          for (var k in obj) {
+            o[k] = clone(obj[k]);
+          }
+        }
+      }
+      break;
+    default: o = obj;
+      break;
+  }
+  return o;
+}
+
+var m1 = clone([1, 2, 3]);
+var m2 = clone('1', 'hello');
+console.log(m1);
+console.log(m2);
 ```
 
+#### 如何消除一个数组中重复的元素
